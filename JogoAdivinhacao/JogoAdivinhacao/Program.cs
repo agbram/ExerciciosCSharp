ef86efaef57b0404a623de36cdcb990e6dbc7d30
Gustavo Braga <agbram2005@gmail.com>
2025-08-20 19:49:21 -0300

    Exercicios realizados em csharp

---
+++ JogoAdivinhacao/JogoAdivinhacao/Program.cs
@@ -0,0 +1,45 @@
+﻿int tentativas = 5;
+int tentativaAtual = 0;
+bool acertou = false;
+
+Random random = new Random();
+int numeroAleatorio = random.Next(1, 51);
+
+Console.WriteLine("Bem-vindo ao jogo de adivinhação!");
+Console.WriteLine($"Você tem {tentativas} tentativas para adivinhar o número entre 1 e 50.");
+
+while (tentativaAtual < tentativas)
+{
+    Console.Write("Digite um número: ");
+    int numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+    if (numeroUsuario < 1 || numeroUsuario > 50)
+    {
+        Console.WriteLine("Número inválido! Digite entre 1 e 50.");
+        continue; // Não conta como tentativa
+    }
+
+    tentativaAtual++;
+
+    if (numeroUsuario == numeroAleatorio)
+    {
+        Console.WriteLine($"Parabéns! Você acertou o número {numeroAleatorio} em {tentativaAtual} tentativas.");
+        acertou = true;
+        break;
+    }
+    else if (numeroUsuario < numeroAleatorio)
+    {
+        Console.WriteLine("O número secreto é MAIOR.");
+    }
+    else
+    {
+        Console.WriteLine("O número secreto é MENOR.");
+    }
+
+    Console.WriteLine($"Tentativas restantes: {tentativas - tentativaAtual}");
+}
+
+if (!acertou)
+{
+    Console.WriteLine($"Suas tentativas acabaram. O número era: {numeroAleatorio}");
+}
---
+++ calculoIMC/calculoIMC/FrmPrincipal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculoIMC
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double altura = Convert.ToDouble(txtAltura.Text);
+            double peso = Convert.ToDouble(txtPeso.Text);
+
+            double imc = peso / (altura * altura);
+
+            ckAbaixo.Checked = false;
+            ckNormal.Checked = false;
+            ckAcima.Checked = false;
+            ckOb1.Checked = false;
+            ckOb2.Checked = false;
+            ckOb3.Checked = false;
+
+            if (imc < 18.5)
+            {
+                ckAbaixo.Checked = true;
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                ckNormal.Checked = true;
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                ckAcima.Checked = true;
+            }
+            else if (imc >= 30 && imc < 35)
+            {
+                ckOb1.Checked = true;
+            }
+            else if (imc >= 35 && imc < 40)
+            {
+                ckOb2.Checked = true;
+            }
+            else
+            {
+                ckOb3.Checked = true;
+            }
+
+            imc = Math.Round(imc, 2);
+
+            txtIMC.Text = imc.ToString();
+
+
+            
+
+        }
+    }
+}
